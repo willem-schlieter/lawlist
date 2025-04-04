45e4ca2f98c8a1d9bdc2f711547615d876aff241
willem-schlieter <jwf.hoffmann@gmx.de>
2025-04-04 17:09:57 +0200

    add MCIDs to prevent LI id confusions in Read Mode

--- main.ts
+++ main.ts
@@ -25,49 +25,91 @@ export default class LawListPlugin extends Plugin {
 		// Add the settings tab for style customisation.
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// Register the view plugin.
+		// Register the view plugin (fror Edit Mode).
 		this.registerEditorExtension(lawlistCMPlugin);
+		
+		// Create and register a MarkdownPostProcessor (for Read Mode).
+		this.registerMarkdownPostProcessor((function(){
+			// We create a closure to host some variables that should be available to the post processor.
+			let mainContainerCounter = 0;
+			// Here we store the stylesheets for every mainContainer according to its assigned id.
+			// Else we would add another <style> for each call of the postProcessor, which might result
+			// in a bad performance/a cluttered DOM.
+			const stylesheets: HTMLStyleElement[] = [];
+			// The problem with this:
+			// I just found out that on every Markdown render, the engine removes the mainContainer
+			// currently modified and rebuilds it. That means: EVERYTIME the postProcessor is run, it
+			// will encounter a newly created mainContainer not touched by the plugin yet. EVERYTIME it is
+			// invoked, a new <style> will be added.
+			// So the whole thing with saving stylesheets in this closure variable to prevent cluttering
+			// the document with houndreds of <style>s does not work. Hopefully, this is not going to
+			// be a performance problem. But I don't know how to prevent it. Doing "garbage collection"
+			// and removing all unused <style>s on every postProcessor call might be even worse for
+			// the performance.
 
-		this.registerMarkdownPostProcessor(mainContainer => {
-			// Contains all LIs with their index and indentation level
-			const listItems: {li: Element, ix: number, level: number}[] = [];
+			return function(mainContainer: HTMLElement): void {
+				// This array will contain all LIs with their index and indentation level
+				const listItems: {li: Element, ix: number, level: number}[] = [];
+				
+				// This block will collect all LIs in the mainContainer.
+				// First, we iterate over all the OLs in the mainContainer.
+				Array.from(mainContainer.querySelectorAll("ol")).forEach(ol => {
+					// Get the indentation level of the OL
+					let level = 0;
+					// The OL itself should have a parent.
+					let parent = ol.parentElement;
+					if (! parent) throw new Error("This OL has no parent.");
+					// Now we have to count the OL/UL elements in the parent chain up to mainContainer.
+					// If the OL's direct parent is the mainContainer, indentation level stays 0.
+					while (parent !== mainContainer) {
+						if (parent === null) throw new Error("No parent found, but still inside mainContainer.");
+						// Should ULs be considered as well? Or should an indentation level due to nesting in UL not count?
+						if (parent.tagName === "OL" || parent.tagName === "UL") level ++;
+						parent = parent.parentElement;
+					}
 
-			// Iterate over all the OLs
-			Array.from(mainContainer.querySelectorAll("ol")).forEach(ol => {
-				// Get the indentation level of the OL
-				let level = 0;
-				// The OL itself should have a parent.
-				let parent = ol.parentElement;
-				if (! parent) throw new Error("This OL has no parent.");
-				// Now we have to count the OL/UL elements in the parent chain up to mainContainer.
-				// If the OL's direct parent is the mainContainer, indentation level stays 0.
-				while (parent !== mainContainer) {
-					if (parent === null) throw new Error("Parent chain interrupted before mainContainer.");
-					// Should ULs be considered as well? Or should an indentation level due to nesting in UL not count?
-					if (parent.tagName === "OL" || parent.tagName === "UL") level ++;
-					parent = parent.parentElement;
+					// Next, get all the LIs that are direct children of that OL
+					const lis = Array.from(ol.children).filter(e => e.tagName === "LI");
+					// Register the LIs to "listItems"
+					lis.forEach((li, index) => listItems.push({li, ix: index + ol.start, level}));
+				});
+
+				// If there are no listItems inside this mainContainer, it is irrelevant to the plugin and can be skipped.
+				if (listItems.length === 0) return;
+				
+				// Else, we need to detect if this mainContainer has already been touched by the plugin.
+				let mcid = Number.parseInt((mainContainer.className.match(/lawlist-mcid-(\d*)/) || [, ""])[1]);
+				// If there is no mainContainer id, this is the first encounter with this mainContainer.
+				if (Number.isNaN(mcid)) {
+					console.log("maincontainer never touched: ", mainContainer);
+					// So we assign an mcid…
+					mcid = mainContainerCounter ++;
+					mainContainer.classList.add("lawlist-mcid-" + mcid);
+					// …and a <style>.
+					stylesheets[mcid] = document.head.appendChild(document.createElement("style"));
 				}
+				// First, we need to give the mainContainer an ID to identify the LIs inside.
+				// (There might be more than one list in the document, resulting in different
+				// mainContainers ("el ol" nodes).)
 
-				// Get all the LIs that are direct children of that OL
-				const lis = Array.from(ol.children).filter(e => e.tagName === "LI");
-				// Register the LIs to "listItems"
-				lis.forEach((li, index) => listItems.push({li, ix: index + ol.start, level}));
-			});
-
-			// Now we have to give every LI a unique ID and style it according to its ix and level
-			// by dynamically generating a <style> sheet adressing all the LIs individually.
-			const stylesheet = document.createElement("style");
-			document.head.appendChild(stylesheet);
-			const sheet = stylesheet.sheet;
-			if (! sheet) throw new Error("Why is there no sheet?");
-			listItems.forEach((item, id) => {
-				// Assign an individual id
-				item.li.classList.add("lawlist-li-id-" + id);
-				// Insert CSS rule to style according to the pattern configured in the settings tab.
-				// (In-Text custom patterns not supported yet.)
-				sheet.insertRule(`.lawlist-li-id-${id}::marker { content: "${parsePattern(store.patterns[item.level] || "1. ", item.ix)}" }`, sheet.cssRules.length);
-			});
-		});
+				// Now we have to give every LI a unique ID and style it according to its ix and level
+				// by dynamically filling the <style> to adress all the LIs individually.
+				const sheet = stylesheets[mcid].sheet;
+				if (! sheet) throw new Error("Why is there no sheet?");
+				// We empty the stylesheet…
+				while (sheet.cssRules.length) sheet.deleteRule(0);
+				// And generate it again. (Else, we would clutter up the stylesheet with unused rules.)
+				listItems.forEach((item, id) => {
+					// Assign an individual id to the LI.
+					// The id class needs to contain the mcid as well, otherwise, there could be several
+					// LIs with the same LI id, as the postProcessor only touches one mainContainer at a time.
+					item.li.classList.add(`lawlist-li-mcid-${mcid}-id-${id}`);
+					// Insert CSS rule to style the LI according to the pattern configured in the settings tab.
+					// (In-Text custom patterns not supported yet.)
+					sheet.insertRule(`.lawlist-li-mcid-${mcid}-id-${id}::marker { content: "${parsePattern(store.patterns[item.level] || "1. ", item.ix)}" }`, sheet.cssRules.length);
+				});
+			}
+		})());
 	}
 
 	onunload() {
@@ -116,7 +158,6 @@ class SampleSettingTab extends PluginSettingTab {
 		desc.appendChild(createEl("code", { text: "1. {a. }Text text text." }));
 		desc.appendText(" would be displayed as ");
 		desc.appendChild(createEl("code", { text: "a. Text text text." }));
-
 
 		for (let i = 0; i < 10; i++ ) {
 			new Setting(containerEl)
